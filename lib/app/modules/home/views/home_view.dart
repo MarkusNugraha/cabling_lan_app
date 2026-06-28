66c5643294f7290bcc40c8a79a9fd8d097c73167
Mark <markus.nugraha.job@gmail.com>
2026-06-28 22:52:25 +0700

    add menu card
    add menu card at home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,6 +6,7 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +16,9 @@ class HomeView extends GetView<HomeController> {
         child: Container(
           // color: Colors.amber[200],
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Profile Section
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -68,6 +71,39 @@ class HomeView extends GetView<HomeController> {
                         ),
                       ],
                     ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 30),
+
+              // Menu Section
+              Text(
+                "Menu",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(height: 5),
+
+              // Menu 1
+              Column(
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: Image.network(
+                          'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Menu 1',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
